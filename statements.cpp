19c8e66c6b615635d25582705c03c130778b9dd0
Manqoba-123 <mlangenimanqoba@gmail.com>
2024-03-04 21:15:31 +0200

    Added another hello world file

--- statements.cpp
+++ statements.cpp
@@ -3,26 +3,28 @@
  * Even numbers are numbers that are divisible by 2 e.g 2 4 6 8 10 etc...
  * Odd numbers are numbers that are not divisible by 2 e.g 1 3 5 7 9 11 etc..
  * prime numbers are numbers divided by 1 and itself only e.g 2,3,5,7,11,13
-*/
+ */
 
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int a = 15;
     int b = 11;
 
-    int modulus = a % b; 
+    int modulus = a % b;
 
+    cout << "The answer = " << modulus << endl;
 
-    cout << "The answer = " << modulus <<endl;
-
-    if (a % 2 == 0) { //if this statement is true you can enter inside the statemnt if false go to else
+    if (a % 2 == 0)
+    { // if this statement is true you can enter inside the statement if false go to else
         cout << a << " is even ";
     }
-    else{
+    else
+    {
         cout << a << " is a gohst";
     }
-    
+
     return 0;
 }
